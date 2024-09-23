3058bedebad8197d524d249814a6429c0c0d05fb
Retford <retfordjb@gmail.com>
2024-09-23 15:39:37 -0500

    Added files

--- useTodos/useTodos.js
+++ useTodos/useTodos.js
@@ -9,7 +9,7 @@ const init = () => {
     : [];
 };
 
-export const useTodo = () => {
+export const useTodos = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState, init);
 
   useEffect(() => {
